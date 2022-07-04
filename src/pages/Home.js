88f63728ea5f1b0882b8935a5f0a5e27e6122742
Your NameR-Lashan <lashan779@gmail.com>
2022-07-04 08:27:00 +0530

    minor changes

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from 'react';
-import image from '../images/coding.jpg';
+import image from '../images/coding.png';
 import './Styles/Home.css';
 import { Link } from 'react-router-dom';
 
